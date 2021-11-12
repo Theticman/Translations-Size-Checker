68f6aa95e29010e2ba33f3726fd12e7e25b047f3
Theticman <41964761+Theticman@users.noreply.github.com>
2021-11-12 16:51:30 +0100

    update

--- js/characterConversion.js
+++ js/characterConversion.js
@@ -24,9 +24,10 @@ async function getImageFromText(text,characterType) {
     let textWidth = 0
     for (let character of text) {
         let {file, row, col, characterSize} = getCharacterPosition(character,characterType)
-        let {canvas, characterWidth} = await getCharacterImage(file,row,col,characterSize)
-        characters.push({canvas, characterWidth})
-        textWidth += characterWidth
+        console.log(file, row, col, characterSize)
+        let{canvas, characterPara} = await getCharacterImage(file,row,col,characterSize)
+        characters.push({canvas, characterPara})
+        textWidth += characterPara.width
     }
     return {characters, textWidth}
 }
@@ -48,7 +49,7 @@ function getCharacterPosition(character,characterType) {
                 col++
                 if (character.charCodeAt(0).toString(16).padStart(4,"0") == unicodeNumber) {
                     let file = provider.file.replace(/minecraft:/, "../assets/")
-                    let characterSize = 8
+                    let characterSize = provider.characterSize
                     return {file,row,col,characterSize}
                 }
             }
@@ -61,28 +62,28 @@ function getCharacterPositionUnicode(unicodeNumber) {
     let file = `../assets/font/unicode_page_${unicodeNumber.substr(0, 2)}.png`
     let row = parseInt(unicodeNumber[2], 16)
     let col = parseInt(unicodeNumber[3], 16)
-    let characterSize = 16
+    let characterSize = {width:16, height:16, ascent:12}
     return {file,row,col,characterSize}
 }
 
 async function getCharacterImage(file,row,col,characterSize) {
-    var canvas = document.createElement("canvas");
-    canvas.width = characterSize+1
-    canvas.height = characterSize+1
+    var canvas = document.createElement("canvas")
     var ctx = canvas.getContext('2d')
-
+    canvas.width = characterSize.width+1
+    canvas.height = characterSize.height+1
+    
     let img = new Image()
     await new Promise((resolve) => {
         img.onload = () => resolve()
         img.src = file
     })
-    ctx.drawImage(img, col*characterSize, row*characterSize, characterSize, characterSize, 0, 0, characterSize, characterSize)
+    ctx.drawImage(img, col*characterSize.width, row*characterSize.height, characterSize.width, characterSize.height, 0, 0, characterSize.width, characterSize.height)
 
-    let characterWidth = -1000
-    for (let i = 0; i<characterSize; i++) {
-        for (let j = 0; j<characterSize; j++) {
+    let characterPara = {width:-1000, ascent:characterSize.ascent}
+    for (let i = 0; i<characterSize.width; i++) {
+        for (let j = 0; j<characterSize.height; j++) {
             if (ctx.getImageData(i,j,1,1).data[0] == 255) {
-                if (i > characterWidth) characterWidth = i
+                if (i > characterPara.width) characterPara.width = i
                 if (ctx.getImageData(i+1,j+1,1,1).data[3] == 0) {
                     let imageData = ctx.getImageData(i,j,1,1)
                     imageData.data[0] = 62
@@ -95,11 +96,11 @@ async function getCharacterImage(file,row,col,characterSize) {
         }
     }
 
-    ctx.drawImage(img, col*characterSize, row*characterSize, characterSize, characterSize, 0, 0, characterSize, characterSize)
+    ctx.drawImage(img, col*characterSize.width, row*characterSize.height, characterSize.width, characterSize.height, 0, 0, characterSize.width, characterSize.height)
     
-    if (characterWidth == -1000) characterWidth = 3
-    else characterWidth++
+    if (characterPara.width == -1000) characterPara.width = 3
+    else characterPara.width++
 
-    return {canvas, characterWidth}
+    return {canvas, characterPara}
 
 }
--- js/script.js
+++ js/script.js
@@ -14,30 +14,53 @@ async function generateImage() {
     const stringTest = document.getElementById("input_text").value
     const characterType = 0
     const UIElementType = 0
+    const scaleRatio = ((characterType == 1) ? 2 : 1)
+    console.log(UIElementsTypes[UIElementType])
+    let UIElement = Object.create(UIElementsTypes[UIElementType])
+    console.log(UIElement)
+    UIElement.width *= scaleRatio
+    UIElement.height *= scaleRatio
+    console.log(UIElement)
 
     const canvas = document.getElementById('canvas')
     const ctx = canvas.getContext('2d')
-
-    const UIElement = UIElementsTypes[UIElementType]
-    const canvasWidth = UIElement.width + 50
-    const canvasHeight = UIElement.height + 50
-    canvas.width = canvasWidth
-    canvas.height = canvasHeight
+    canvas.width = UIElement.width + 50
+    canvas.height = UIElement.height
 
     // Load UI element
     let UIImage = new Image()
     UIImage.src = `../assets/UIElements/${UIElement.name}.png`
     UIImage.onload = function() {
-        ctx.drawImage(UIImage, 25, 25)
+        ctx.imageSmoothingEnabled = false;
+        ctx.drawImage(UIImage, canvas.width/2 - UIElement.width/2, canvas.height/2 - UIElement.height/2, UIElement.width, UIElement.height)
     }
 
     // Convert characters to images
     let {characters, textWidth} = await getImageFromText(stringTest,characterType)
-    let cursor = Math.floor(canvasWidth/2 - textWidth/2 - characters.length/2)
+    let cursor = Math.floor(canvas.width/2 - textWidth/2 - characters.length/2)
 
     // Place character
     for (let character of characters) {
-        ctx.drawImage(character.canvas, cursor, canvasHeight/2 - 4)
-        cursor += character.characterWidth + 1
+        ctx.drawImage(character.canvas, cursor, Math.ceil(canvas.height/2 - character.characterPara.ascent+3),character.canvas.width,character.canvas.height)
+        cursor += character.characterPara.width + 1
     }
+    resizeCanvas()
+}
+
+function resizeCanvas() {
+    var myCanvas = document.getElementById('canvas');
+    var tempCanvas = document.createElement('canvas');
+    tempCanvas.width = myCanvas.width;
+    tempCanvas.height = myCanvas.height;
+
+    // save your canvas into temp canvas
+    tempCanvas.getContext('2d').drawImage(myCanvas, 0, 0);
+
+    // resize my canvas as needed, probably in response to mouse events
+    myCanvas.width *= 5.2333
+    myCanvas.height *= 5.2333
+
+    // draw temp canvas back into myCanvas, scaled as needed
+    myCanvas.getContext('2d').imageSmoothingEnabled = false;
+    myCanvas.getContext('2d').drawImage(tempCanvas, 0, 0, tempCanvas.width, tempCanvas.height, 0, 0, myCanvas.width, myCanvas.height);
 }
