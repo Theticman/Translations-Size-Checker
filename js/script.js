9a710d9002ce9ad2003f09defa16120091d36c3f
Theticman <41964761+Theticman@users.noreply.github.com>
2021-11-17 21:57:10 +0100

    hope

--- js/script.js
+++ js/script.js
@@ -71,3 +71,40 @@ function generateInit() {
 // Ticking (loop to check for update every 100ms)
 let inputedText = ""
 setInterval(generateInit, 100)
+
+window.onload=function(){
+    const element = document.querySelector("#container");
+
+    element.addEventListener('wheel', (event) => {
+        event.preventDefault();
+
+        element.scrollBy({
+            left: event.deltaY < 0 ? -70 : 70,
+        
+        });
+    });
+    for (let UIElement of UIElementsTypes) {
+        var node = document.createElement("uielement-card")
+        node.setAttribute("name", UIElement.name)
+        node.setAttribute("type", UIElement.type)
+        node.setAttribute("icon", `{{ site.baseurl }}/../assets/UIElements/${UIElement.src}`)
+        node.setAttribute("id", UIElement.id)
+        node.setAttribute("onclick", "selectUIElement(this.getAttribute('id'))")
+        element.appendChild(node)
+        if (UIElement.id == UIElementType) node.firstElementChild.classList.add("selected")
+    }
+    selectUIElement(0);
+}
+
+function selectUIElement(index) {
+    // remove selected class
+    document.querySelector(".selected").classList.remove("selected")
+
+    // add selected class
+    document.getElementById(`${index}`).firstElementChild.classList.add("selected");
+
+    UIElementType = index
+    inputedText = document.getElementById("input_text").value
+    inputedText = (inputedText == "") ? "Input text..." : inputedText
+    generateImage(inputedText)
+}
